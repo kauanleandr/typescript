5aaf392722a9b7680a71f584e1afcd590afe0af7
kauanleandr <kauanprograma@gmail.com>
2024-11-06 17:04:15 -0300

    finalizando projetinho

--- dist/index.js
+++ dist/index.js
@@ -13,15 +13,34 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+const pokemons = [
+    "Bulbasaur", "Charmander", "Squirtle", "Pikachu", "Jigglypuff",
+    "Meowth", "Psyduck", "Machop", "Geodude", "Onix",
+    "Abra", "Gengar", "Eevee", "Snorlax", "Mewtwo",
+    "Chikorita", "Cyndaquil", "Totodile", "Togepi", "Mareep",
+    "Espeon", "Umbreon", "Scizor", "Heracross", "Tyranitar",
+    "Treecko", "Torchic", "Mudkip", "Ralts", "Gardevoir",
+    "Sableye", "Mawile", "Aggron", "Flygon", "Salamence",
+    "Metagross", "Turtwig", "Chimchar", "Piplup", "Lucario",
+    "Garchomp", "Munchlax", "Electivire", "Magmortar", "Togekiss",
+    "Gliscor", "Snivy", "Tepig", "Oshawott", "Zoroark",
+    "Excadrill", "Hydreigon", "Volcarona", "Genesect", "Chespin",
+    "Fennekin", "Froakie", "Talonflame", "Sylveon", "Goodra",
+    "Rowlet", "Litten", "Popplio", "Lycanroc", "Toxapex",
+    "Mimikyu", "Grookey", "Scorbunny", "Sobble", "Corviknight",
+    "Duraludon", "Dragapult"
+];
 app.get('/', (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        res.send("Hello World");
+        res.json(pokemons);
     }
     catch (error) {
         res.status(500).send("Ocorreu um erro.");
     }
 }));
-app.listen(3333, () => {
-    console.log('Server funcionando http://localhost:3333');
+app.listen(4444, () => {
+    console.log('Servidor funcionando na porta 4444.');
 });
--- srv/index.ts
+++ srv/index.ts
@@ -1,15 +1,35 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors()); 
+
+const pokemons: string[] = [
+    "Bulbasaur", "Charmander", "Squirtle", "Pikachu", "Jigglypuff", 
+    "Meowth", "Psyduck", "Machop", "Geodude", "Onix",
+    "Abra", "Gengar", "Eevee", "Snorlax", "Mewtwo", 
+    "Chikorita", "Cyndaquil", "Totodile", "Togepi", "Mareep",
+    "Espeon", "Umbreon", "Scizor", "Heracross", "Tyranitar", 
+    "Treecko", "Torchic", "Mudkip", "Ralts", "Gardevoir", 
+    "Sableye", "Mawile", "Aggron", "Flygon", "Salamence", 
+    "Metagross", "Turtwig", "Chimchar", "Piplup", "Lucario",
+    "Garchomp", "Munchlax", "Electivire", "Magmortar", "Togekiss", 
+    "Gliscor", "Snivy", "Tepig", "Oshawott", "Zoroark", 
+    "Excadrill", "Hydreigon", "Volcarona", "Genesect", "Chespin", 
+    "Fennekin", "Froakie", "Talonflame", "Sylveon", "Goodra", 
+    "Rowlet", "Litten", "Popplio", "Lycanroc", "Toxapex", 
+    "Mimikyu", "Grookey", "Scorbunny", "Sobble", "Corviknight", 
+    "Duraludon", "Dragapult"
+];
 
 app.get('/', async (_req, res) => {
     try {
-        res.send("Hello World");
+        res.json(pokemons);
     } catch (error) {
         res.status(500).send("Ocorreu um erro.");
     }
 });
 
-app.listen(3333, () => {
-    console.log('Server funcionando http://localhost:3333');
+app.listen(4444, () => {
+    console.log('Servidor funcionando na porta 4444.');
 });
